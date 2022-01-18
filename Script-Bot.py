54f4528d7695cf402bced4a733699aba0c999005
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2022-01-18 05:16:04 +0000

    Add files via upload

---
+++ Script-Bot.py
@@ -0,0 +1,104 @@
+import os,sys,time
+os.system("clear")
+def nanya():
+    a = raw_input("do you want more? [Y/T] ~~> ")
+    if a =="Y" or a =="y":
+	os.system("python2 Script-Bot.py")
+    elif a =="T" or a =="t":
+        os.system("clear")
+        os.system("figlet Thank You For Using This Script")
+print
+banner = """\033[1;96m ___  ____ ___  ____ ____ ____ _ ___  ___
+ |__] |  |  |   [__  |    |__/ | |__]  |
+ |__] |__|  |   ___] |___ |  \ | |     |
+"""
+print
+print (banner)
+print
+print ("\033[1;\033[1;90m1. \033[1;92mLord Botz")
+print ("\033[1;\033[1;90m2. \033[1;92mAlpha Botz")
+print
+pilihgan = input("\033[1;\033[1;90mInput Option : ")
+if pilihgan == 1:
+        print ("\033[1;\033[1;90mStarting.....")
+        print ("""\033[1;93m1.handler.js
+2.index.js
+3.test.js
+4.server.js
+5.resetuser.js
+6.package.json
+7.package-lock.json
+8.Dockerfile
+9.LICENSE
+10.Procfile
+11.app.json
+12.README.md
+13.config.js (Bagian Penyetingan Owner)
+14.database.json (Free 300 Database Lord Botz)
+15.install.sh (Untuk Install Module)
+16.main.js
+17.lib (Folder Format)
+18.plugins (Folder Format)
+19.src (Folder Format)
+20.tmp (Folder Format)
+21.views (Folder Format)
+""")
+pil = input("\033[1;\033[1;90mYour Options : ")
+if pil == "":
+	os.system("pytho2 Script-Bot.py")
+if pil == 1:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/handler.js")
+	nanya()
+if pil == 2:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/index.js")
+	nanya()
+if pil == 3:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/test.js")
+	nanya()
+if pil == 4:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/server.js")
+	nanya()
+if pil == 5:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/resetuser.js")
+	nanya()
+if pil == 6:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/package.json")
+	nanya()
+if pil == 7:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/package-lock.json")
+	nanya()
+if pil == 8:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/Dockerfile")
+	nanya()
+if pil == 9:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/LICENSE")
+	nanya()
+if pil == 10:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/Procfile")
+	nanya()
+if pil == 11:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/app.json")
+	nanya()
+if pil == 12:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/README.md")
+	nanya()
+if pil == 13:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/config.js")
+	nanya()
+if pil == 14:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/database.json")
+	nanya()
+if pil == 15:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/install.sh")
+	nanya()
+if pil == 16:
+	os.system("xdg-open https://raw.githubusercontent.com/AmmarBN/lordbot-aq-tes/master/main.js")
+	nanya()
+
+if pilihgan == 2:
+      print ("Coming Soon")
+      time.sleep(5)
+      os.system("clear")
+      os.system("figlet Thank You For Using This Script")
+	    
+	
